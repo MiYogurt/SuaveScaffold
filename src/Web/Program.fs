e05eadf11c1f7d9cfb92b5bc81a30bfb1a24adac
ChenMingMing <belovedyogurt@gmail.com>
2019-04-01 16:24:27 +0800

    🚗 Add router map

--- src/Web/Program.fs
+++ src/Web/Program.fs
@@ -1,10 +1,25 @@
 ﻿open System
 open Model
 open Suave
+open Suave.Successful
+open Suave.Filters
+open Suave.Operators
+open Suave.Sockets
+open Model
+
+let dbHandler : WebPart = (fun ctx -> 
+   let blog = Utils.getBlog()
+   OK blog.Url ctx
+)
+
+let router : WebPart = choose [
+    GET >=> path "/" >=> OK "hello world"
+    GET >=> path "/db" >=> dbHandler
+    
+]
 
 [<EntryPoint>]
 let main argv =
-    let blog = Utils.getBlog()
-    startWebServer defaultConfig (Successful.OK blog.Url)
+    startWebServer defaultConfig router
 
     0 // return an integer exit code
